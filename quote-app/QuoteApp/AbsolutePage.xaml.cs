ecad1f00938d2410aea9deeb03fe5348b06c3b62
Christian Arce <carce@perceptiongrp.com>
2018-05-07 14:02:06 -0300

    absolute layout

--- quote-app/QuoteApp/AbsolutePage.xaml.cs
+++ quote-app/QuoteApp/AbsolutePage.xaml.cs
@@ -10,6 +10,21 @@ namespace QuoteApp
         public AbsolutePage()
         {
             InitializeComponent();
+
+            var layout = new AbsoluteLayout();
+            Content = layout;
+
+            var aquaBox = new BoxView()
+            {
+                Color = Color.Aqua
+            };
+
+
+            layout.Children.Add(aquaBox, new Rectangle(0, 0, 1, 1), AbsoluteLayoutFlags.All);
+
+            AbsoluteLayout.SetLayoutBounds(aquaBox, new Rectangle(0, 0, 1, 1));
+            AbsoluteLayout.SetLayoutFlags(aquaBox, AbsoluteLayoutFlags.HeightProportional);
+
         }
     }
 }
